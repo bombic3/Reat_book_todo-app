e1430e89c2520f1903423ca57b615b286ee97670
bombi <bombic3@naver.com>
2022-07-04 12:09:38 +0900

    FEAT : 추가 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,12 +1,60 @@
+import { useCallback, useRef, useState } from "react";
 import TodoInsert from "./components/TodoInsert";
 import TodoList from "./components/TodoList";
 import TodoTemplate from "./components/TodoTemplate";
 
+// todos 배열에 새 객체 추가하기
+// - App 컴포넌트에서 todos 배열에 새 객체 추가하는 onInsert 함수 만들기
+// - 이 함수에선 새로운 객체 만들 때마다 id 값에 1씩 더해줘야 함
+// - id 값은 useRef 사용하여 관리
+// - 여기서 useState가 아닌 useRef 사용해 컴포넌트에서 사용할 변수를 만드는 이유
+//     - id 값은 렌더링되는 정보가 아니기 때문. 단순히 참조되는 값
+// - onInsert 함수는 컴포넌트의 성능 아낄 수 잇도록 useCallback 으로 감싸주기
+//     - props로 전달해야 할 함수 만들 때는 useCallback 사용해 함수 감싸는 것 습관화해주기
+// - onInsert 함수 만든 뒤 해당 함수를 TodoInsert 컴포넌트의 props로 설정
+
 const App = () => {
+  const [todos, setTodos] = useState([
+    {
+      // map 사용 위해 고유한 key 값 id 넣어줌
+      id: 1,
+      text: '리액트의 기초 알아보기',
+      checked: true,
+    },
+    {
+      id: 2,
+      text: '컴포넌트 스타일링 해보기',
+      checked: true,
+    },
+    {
+      id: 3,
+      text: '쇼핑몰 만들어 보기',
+      checked: false,
+    },
+  ]);
+
+  // 고유값으로 사용될 id
+  // ref를 사용하여 변수 담기
+  const nextId = useRef(4);
+
+  const onInsert = useCallback(
+    text => {
+      const todo = {
+        id: nextId.current,
+        text,
+        checked: false,
+      };
+      setTodos(todos.concat(todo));
+      nextId.current += 1; // nextId 1씩 더하기
+    },
+    [todos],
+  );
+  
   return (
     <TodoTemplate>
-      <TodoInsert />
-      <TodoList />
+      {/* <TodoInsert /> */}
+      <TodoInsert onInsert={onInsert} />
+      <TodoList todos={todos} />
     </TodoTemplate>
   );
 };
--- src/components/TodoInsert.js
+++ src/components/TodoInsert.js
@@ -1,11 +1,39 @@
+import { useCallback, useState } from 'react';
 import { MdAdd } from 'react-icons/md';
 import './TodoInsert.scss'
 
-const TodoInsert = () => {
+// App 에서 TodoInsert에 넣어 준 onInsert 함수에
+// 현재 useState를 통해 관리하고 있는 value 값을
+// 파라미터로 넣어서 호출
+const TodoInsert = ({ onInsert }) => {
+  const [value, setValue] = useState('');
+
+  const onChange = useCallback(e => {
+    // console.log("OK");
+    setValue(e.target.value);
+  }, []);
+
+  // onSubmit을 useCallback사용하여 생성
+  const onSubmit = useCallback(
+    e => {
+      onInsert(value);
+      setValue(''); // value 값 초기화
+
+      // submit 이벤트는 브라우저에서 새로고침을 발생시킴
+      // 이를 방지하기 위해 이 함수 호출함
+      e.preventDefault();
+    },
+    [onInsert, value],
+  );
+  
   return (
     <div>
-      <form className='TodoInsert'>
-        <input placeholder='할 일을 입력하세요' />
+      <form className='TodoInsert' onSubmit={onSubmit}>
+        <input
+          placeholder='할 일을 입력하세요'
+          value={value}
+          onChange={onChange}
+        />
         <button type='submit'>
           <MdAdd />
         </button>
@@ -15,3 +43,34 @@ const TodoInsert = () => {
 };
 
 export default TodoInsert;
+
+
+// - onSubmit 대신에 button의 onClick 이벤트로 처리 가능하지만 form 과 onSubmit 사용한 이유
+// → onSubmit 이벤트의 경우 인풋에서 Enter를 눌렀을 때로 발생하기 때문
+// → 버튼에서 onClick만 사용했다면 인풋에서 onKeyPress 이벤트 통해 Enter 감지하는 로직 따로 만들어야 함
+
+// onSubmit 대신에 button의 onClick 이벤트로 처리 했을 시 코드
+/*
+
+const onClick = useCallback(
+  () => {
+    onInsert(value);
+    setValue('');
+  },
+  [onInsert, value],
+);
+
+return (
+  <form className='TodoInsert'>
+    <input
+      placeholder='할 일을 입력하세요'
+      value={value}
+      onChange={onChange}
+    />
+    <button onClick={onClick}>
+      <MdAdd />
+    </button>
+  </form>
+);
+
+*/
