eaa42d9a09d63a9c3204e974c46598c0864ffd98
bombi <bombic3@naver.com>
2022-07-06 13:48:37 +0900

    CHORE : 2.useReducer 사용하여 함수 재 생성반복 방지하기

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import { useCallback, useRef, useState } from "react";
+import { useCallback, useReducer, useRef } from "react";
 import TodoInsert from "./components/TodoInsert";
 import TodoList from "./components/TodoList";
 import TodoTemplate from "./components/TodoTemplate";
 
-// 많은 데이터 렌더링하기
-// - 데이터 하나하나 직접 입력하지 않고 createBulkTodos 함수 만들어 데이터 2,500개 자동 생성하도록 함
-// - 주의할 점 : useState 기본값에 함수를 넣어줬다는 것
-//     → useState (createBulkTodos()) 라고 적으면 리렌더링 될 때 마다 createBulkTodos함수 호출 됨
-//     → useState(createBulkTodos) 처럼 파라미터를 함수 형태로 넣어주면 컴포넌트가 처음 렌더링 될 때만 createBulkTodos 함수 실행
+/*
+### useReducer 사용하기
+- useReducer → 두 번째 파라미터 = 초기 상태 넣어줘야 함
+- 지금은 그 대신 두 번재 파라미터에 undefined를 넣고, 세 번째 파라미터에 초기 상태 만들어 주는 함수인 createBulkTodos를 넣어 줌
+    → 컴포넌트가 맨 처음 렌더링될 때만 createBulkTodos 함수 호출 됨
+    
+- useReducer 사용 → 기존 코드 많이 고쳐야 하는 단점
+    BUT → 상태를 업데이트하는 로직 모아서 컴포넌트 바깥에 둘 수 있다는 장점
+    (성능상 useState 함수형 업데이트와 비슷함 취향에 따라 사용)
+*/
 
 function createBulkTodos () {
   const array = [];
@@ -21,33 +26,32 @@ function createBulkTodos () {
   return array;
 }
 
-const App = () => {
-  const [todos, setTodos] = useState(createBulkTodos);
+// 2. useReducer 사용하기
+function todoReducer (todos, action) {
+  switch (action.type) {
+    case 'INSERT': // 추가
+      // { type: 'INSERT', todo: { id: 1, test: 'todo', checked: false }}
+      return todos.concat(action.todo);
+    case 'REMOVE': // 제거
+      // { type: 'REMOVE', id: 1 }
+      return todos.filter(todo => todo.id !== action.id);
+    case 'TOGGLE': // 토글
+      // { type: 'TOGGLE', id: 1 }
+      return todos.map(todo =>
+        todo.id === action.id ? { ...todo, checked: !todo.checked } : todo,
+      );
+    default:
+      return todos;
+  }
+}
 
-  // const [todos, setTodos] = useState([
-  //   {
-  //     id: 1,
-  //     text: '리액트의 기초 알아보기',
-  //     checked: true,
-  //   },
-  //   {
-  //     id: 2,
-  //     text: '컴포넌트 스타일링 해보기',
-  //     checked: true,
-  //   },
-  //   {
-  //     id: 3,
-  //     text: '쇼핑몰 만들어 보기',
-  //     checked: false,
-  //   },
-  // ]);
-  
-  
+const App = () => {
+  // const [todos, setTodos] = useState(createBulkTodos);
+  const [todos, dispatch] = useReducer(todoReducer, undefined, createBulkTodos);
+
   // 고유값으로 사용될 id
   // ref 사용하여 변수 담기
   const nextId = useRef(2501);
-  
-  // const nextId = useRef(4);
 
   const onInsert = useCallback( text => {
       const todo = {
@@ -55,30 +59,19 @@ const App = () => {
         text,
         checked: false,
       };
-      // setTodos(todos.concat(todo));
-      setTodos(todos => todos.concat(todo));
+      dispatch({ type: 'INSERT', todo });
       nextId.current += 1; // nextId 1씩 더하기
-    }, []
-    // [todos],
-  );
+    }, []);
   
   // onRemove 작성
   const onRemove = useCallback( id => {
-      setTodos(todos => todos.filter(todo => todo.id !== id));
-    }, []
-    // [todos],
-  );
+    dispatch({ type: 'REMOVE', id });
+    }, []);
 
   // onToggle 작성
   const onToggle = useCallback( id => {
-      setTodos(todos =>
-        todos.map(todo =>
-          todo.id === id ? { ...todo, checked: !todo.checked } : todo,
-        ),
-      );
-    }, []
-    // [todos],
-  );
+    dispatch({ type: 'TOGGLE', id });
+    }, []);
 
   return (
     <TodoTemplate>
@@ -90,44 +83,3 @@ const App = () => {
 };
 
 export default App;
-
-/*
-## React DevTools를 사용한 성능 모니터링
-
-- 정확히 몇 초가 걸리는지 확인 → React DevTools 사용
-- 리액트 v17 이전 → 브라우저 내장 성능 측정 도구 User Timing API 사용
-- 리액트 v17 이후 → 리액트 전용 개발자 도구 React DecTools 사용
-    
-    → 개발자 도구 창(F12) Profiler 탭 클릭
-    
-    → 좌측 상단 파란색 녹화 버튼
-    
-    → 불꽃 모양 아이콘과 랭크 차트 아이콘 눌러보기
-    
-    → 리렌더링 된 시간과, 리렌더링 된 컴포넌트 오래 걸린 순으로 정렬해서 보여줌
-
-→ 컴포넌트 리렌더링 성능 최적화 → 불필요한 리렌더링 방지
-
-## onToggle, onRemove 함수가 바뀌지 않게 하기
-
-- React.memo를 사용하는 것만으로 최적화 끝 아님
-- 현재 프로젝트에서 todos 배열 업데이트 시 onRemove와 onToggle 함수도 새로 바뀌기 때문
-- onRemove와 onToggle 함수는 배열 상태를 업데이트 하는 과정에서 최신 상태의 todos를 참조하기 때문에 todos 배열이 바뀔 때마다 함수가 새로 만들어짐
-
-### 함수가 계속 만들어지는 상황 방지하는 두 가지 방법
-
-1. useState의 함수형 업데이트 기능 사용
-2. useReducer 사용
-
-
-### useState의 함수형 업데이트
-
-- 기존 - setTodos 함수를 사용할 때 새로운 상태를 파라미터로 넣어 주었음
-- 변경 - setTodos를 사용할 때 새로운 상태를 파라미터로 넣는 대신, 상태 업데이트를 어떻게 할지 정의해 주는 업데이트 함수를 넣기 → 함수형 업데이트
-
-### onToggle, onRemove 함수에서 1️⃣ useState의 함수형 업데이트 사용해보기
-
-- 이 과정에서 onInsert 함수도 함께 수정
-- setTodos를 사용할 때 그 안에 todos ⇒ 만 앞에 넣어주면 됨
-- 수정 후 Profiler 개발자 도구 확인하면 빨라져 있음
-*/
