ed10b7826a06bf95c139c9a0c97ef3de6c955ad8
bombi <bombic3@naver.com>
2022-07-07 10:08:08 +0900

    CHORE : TodoList.js React.memo사용해 미리 최적화

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,5 +1,19 @@
 import TodoListItem from './TodoListItem';
 import './TodoList.scss';
+import React from 'react';
+/*
+## TodosList 컴포넌트 최적화하기
+- 리스트 관련 컴포넌트 최적화 할 시
+    → 리스트 내부에서 사용하는 컴포넌트와
+    → 리스트로 사용되는 컴포넌트 자체 최적화 시켜주기
+
+→ 지금 최적화 코드는 현재 프로젝트 성능에 전혀 영향 주지 않음
+→ TodoList 컴포넌트의 부모 컴포넌트 App 컴포넌트가 리렌더링 되는 유일한 이유가 todos 배열 업데이트 시이기 때문
+→ 즉, 지금 TodoList 컴포넌트는 불필요한 리렌더링 발생하지 않음
+→ 하지만 App 컴포넌트에 다른 state가 추가되어 해당 값들이 업데이트될 때는 TodoList 컴포넌트가 불필요한 리렌더링을 할 수도 있음
+→ 때문에 미리 React.memo 사용해 미치 최적화
+ */
+
 
 const TodoList = ({ todos, onRemove, onToggle }) => {
   return (
@@ -16,4 +30,4 @@ const TodoList = ({ todos, onRemove, onToggle }) => {
   );
 };
 
-export default TodoList;
+export default React.memo(TodoList);
